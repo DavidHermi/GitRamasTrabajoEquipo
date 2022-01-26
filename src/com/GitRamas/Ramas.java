d0b198c5b7cb86fb75f31c425c60c247d9441480
DavidHermi <dhermidaperez@danielcastelao.org>
2022-01-26 14:08:45 +0100

    Segundo commit rama (D)

--- src/com/GitRamas/Ramas.java
+++ src/com/GitRamas/Ramas.java
@@ -3,8 +3,8 @@ package com.GitRamas;
 public class Ramas {
     public void ramas (){
 
-        System.out.println("Primer commit segunda rama D" );
-
+        System.out.println("Primer commit segunda rama C" );
+        System.out.println("Segundo commit segunda rama D");
 
 
     }
