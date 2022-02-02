4011de8db42e3f12593da3349f307fb099d98c70
DavidHermi <dhermidaperez@danielcastelao.org>
2022-02-02 13:20:38 +0100

    cuarto commit rama (f)

--- src/com/GitRamas/Main.java
+++ src/com/GitRamas/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Segundo commit B");
-        System.out.println("Segundo commit E");
+        System.out.println("Tercer commit E");
+        System.out.println("cuarto commit f");
     }
 }
